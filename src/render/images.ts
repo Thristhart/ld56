84b9e266e2a1cfd3806f26b89107749f4c86d878
gqycloud <gqycloud@gmail.com>
2024-10-04 22:04:05 -0700

    ugly mouse

--- src/render/images.ts
+++ src/render/images.ts
@@ -1,9 +1,13 @@
 import turtlePortraitUrl from "~/assets/turtle_portrait.png";
+import mousePortraitUrl from "~/assets/mouse_portrait.png";
 import dialogBackgroundUrl from "~/assets/dialog_panel.png";
 import { EntityType } from "~/game/levels";
 
 export const turtlePortraitImage = new Image();
 turtlePortraitImage.src = turtlePortraitUrl;
+
+export const mousePortraitImage = new Image();
+mousePortraitImage.src = mousePortraitUrl;
 
 export const dialogBackgroundImage = new Image();
 dialogBackgroundImage.src = dialogBackgroundUrl;
@@ -12,6 +16,7 @@ dialogBackgroundImage.src = dialogBackgroundUrl;
 export function GetEntityPortrait(entity: EntityType) {
     switch (entity) {
         case 'turtle': return turtlePortraitImage;
+        case 'mouse': return mousePortraitImage;
         default: return '';
     }
 }
