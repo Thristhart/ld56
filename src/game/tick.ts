8f8699e52b57788758ce1980fd85a183909e767c
Tom Shea <thshea@microsoft.com>
2024-10-04 21:15:51 -0700

    simple input system

--- src/game/tick.ts
+++ src/game/tick.ts
@@ -1,10 +1,12 @@
 import { drawFrame } from "~/render/drawframe";
+import { tickInput } from "./input";
 
 let lastTickTime = performance.now();
 let animationFrame: number;
 export function tick(timestamp: number) {
     const dt = timestamp - lastTickTime;
     lastTickTime = timestamp;
+    tickInput(timestamp);
     drawFrame();
     animationFrame = requestAnimationFrame(tick);
 }
