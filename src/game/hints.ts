eb2ba94c7cfcfc9ee3dec78a9504532bf731be0d
Zyrconium <josef.hill@googlemail.com>
2024-10-07 18:40:20 +0100

    Rudimentary hinting.

--- src/game/hints.ts
+++ src/game/hints.ts
@@ -22,42 +22,45 @@ const levelHints: Record<keyof typeof levels, { count: number, hints: string[] }
             'hint five'
         ]
     },
-    riverCrossing: {
-        count: 0, hints: [
-            'hint one',
-            'hint two',
-            'hint three',
-            'hint four',
-            'hint five'
-        ]
-    },
     flower1: {
         count: 0, hints: [
-            'hint one',
-            'hint two',
-            'hint three',
-            'hint four',
-            'hint five'
+            'Hop on!',
+            'I can\'t follow you in that tunnel.',
+            'Look it\'s a block moving puzzle, you know those, right?',
+            'Isolate the middle and then remove it? Something like that?',
+            'Buttons open doors?'
         ]
     },
     flower2: {
         count: 0, hints: [
-            'hint one',
-            'hint two',
-            'hint three',
-            'hint four',
-            'hint five'
+            '[Bird] can reach that button.',
+            '[Frog] likes to eat bugs.',
+            'Let\'s both get to the left together.',
         ]
     },
     boulderPond: {
         count: 0, hints: [
-            'hint one',
-            'hint two',
-            'hint three',
-            'hint four',
-            'hint five'
+            'We\'re going to have to split up for a second.',
+            'We\'ll have to approach this red button from the left.',
+            '[Turtle] can carry rocks in the water.',
+            'You\'ll need to approach the blue button from directly below it.',
         ]
     },
+    riverCrossing: {
+        count: 0, hints: [
+            'Buttons and Doors are color matched.',
+            'You can fly! (And should)',
+            'You\'re only going to need to eat 10 insects. (2 sets, convey that however you want)',
+            'You\'re going to need every boulder.',
+        ]
+    },
+    superBoulderAdventure:{
+        count: 0, hints: [
+            '[Frog] won\'t be able to get to our destination without this bridge here.',
+            'We\'re going to need to get this boulder all the way over there.',
+            'Oh, could [Frog] clear the way for me up there?',
+        ]
+    }
 }
 export function GetHint(levelname: keyof typeof levels) {
     const levelHint = levelHints[levelname];
